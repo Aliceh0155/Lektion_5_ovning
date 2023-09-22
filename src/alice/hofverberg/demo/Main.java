0e45bc7b46b6eb219cb1326620860d56a0dbe986
aliceh0155 <hofverbergalice@gmail.com>
2023-09-22 10:21:00 +0200

    Lektion_5_uppgift1

--- src/alice/hofverberg/demo/Main.java
+++ src/alice/hofverberg/demo/Main.java
@@ -1,9 +1,16 @@
 package alice.hofverberg.demo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("App is running!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input player name:");
+
+        String namn = sc.nextLine();
+        System.out.println("Player #1: " + namn);
 
     }
 }
